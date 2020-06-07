bdd28d49d4a73b8cfa410df058e3890d8a9e0d27
Brenda-M <brendanjuguna1@gmail.com>
2020-06-07 17:49:37 +0300

    add a save project function

--- projects/models.py
+++ projects/models.py
@@ -3,7 +3,14 @@ from django.db import models
 class Project(models.Model):
   title = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
-  image = models.ImageField(upload_to='/projects')
+  image = models.ImageField(upload_to='projects')
   link = models.URLField(max_length=200)
+
+  def __str__(self):
+    return self.title
+
+  def save_project(self):
+    return self.save()
+
   
 
