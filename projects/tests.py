6607f1433fe6e88f77bf596e2ce76f6c3f0e1993
Brenda-M <brendanjuguna1@gmail.com>
2020-06-07 17:49:04 +0300

    set up a test for save function

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Project
 
-# Create your tests here.
+class ProjectTestClass(TestCase):
+
+  def setUp(self):
+    self.project_1 = Project(title='Test Title', description='This is a test class for the project model', image='test.jpg', link='www.testimage.com')
+
+  def test_save(self):
+    return self.project_1.save_project()
+  
+
+
