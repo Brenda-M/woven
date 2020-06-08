2dc6e09bdaf33e9d12b1a3880aeedabe6553cd73
Brenda-M <brendanjuguna1@gmail.com>
2020-06-08 23:28:53 +0300

    add project api view

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import ProjectSerializer
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Project
 from .forms import CreateNewForm
@@ -20,6 +22,20 @@ class PostCreateView(CreateView):
   model = Project
   form_class = CreateNewForm
   template_name = 'projects/new_post.html'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class ProjectViewSet(viewsets.ModelViewSet):
+  queryset = Project.objects.all().order_by('- created_at')
+  serializer_class = ProjectSerializer
+
+
+
+
+
+
 
  
 
