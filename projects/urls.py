499776fe077c9dde0de9e765764a986b341d1907
Brenda-M <brendanjuguna1@gmail.com>
2020-06-08 19:21:01 +0300

    modify detailed view

--- projects/urls.py
+++ projects/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
   path('', PostListView.as_view(), name='homepage'), 
-  path('inspiration/<int:id>/', PostDetailView.as_view(), name='post-detail' )
+  path('inspiration/<int:pk>/', PostDetailView.as_view(), name='post-detail' )
 ]
