952bd42cd1b29eea3f0f563be7f5b7599b816785
Brenda-M <brendanjuguna1@gmail.com>
2020-06-08 16:44:52 +0300

    add a class based view to display posts

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Project
+
+class PostListView(ListView):
+  projects = Project.objects.all()
+  model = Project
+  template_name = 'projects/main.html'
+  context_object_name = 'projects'
+  ordering = ['-id']
 
 
 
-def home(request):
-  return render(request, 'projects/main.html')
