aeea0171fa31c8b1f84615e0c824fd2b50627338
Brenda-M <brendanjuguna1@gmail.com>
2020-06-08 22:57:29 +0300

    add a urls path for new post

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from . import views
 
 urlpatterns = [
   path('', PostListView.as_view(), name='homepage'), 
-  path('inspiration/<int:pk>/', PostDetailView.as_view(), name='post-detail' )
+  path('inspiration/<int:pk>/', PostDetailView.as_view(), name='post-detail')
+  path('new/post/', PostCreateView.as_view(), name='new-post')
 ]
