97d90380b346989581de4eaee8036a4e7eb632c0
Brenda-M <brendanjuguna1@gmail.com>
2020-06-11 10:29:01 +0300

    remove resizing functionality

--- users/models.py
+++ users/models.py
@@ -14,17 +14,5 @@ class Profile(models.Model):
     return f'{self.user.username} Profile'
 
 
-#   #resizing images..not the most efficient
-  
-  def save(self, *args, **kwargs):
-    super(Profile, self).save(*args, **kwargs)
-
-    img = Image.open(self.prof_pic.path)
-    if img.height> 300 or img.width > 300:
-      output_size = (300, 300)
-      img.thumbnail(output_size)
-      img.save(self.prof_pic.path)
-
-
 
 
